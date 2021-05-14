783385adfa72830272de445293169e793072cada
Doug Pace <whirly1227@gmail.com>
2021-05-14 09:58:57 -0400

    Plot files only
    Added some code to only move .plot files. No longer uses 4th arg for file move time. Once a plot file is complete, it's moved based on the Delay time for checking files.

--- FileMover/Program.cs
+++ FileMover/Program.cs
@@ -10,18 +10,25 @@ namespace FileMover
 		static string SourceDirectory = "G:\\";
 		static string MoveToDirectory = "I:\\";
 		static int DelayTime = 10;
-		static int FileMoveTime = 3;
+		//static int FileMoveTime = 1;
 		static void Main(string[] args)
 		{
-			if (args[0] != null) SourceDirectory = args[0];
-			if (args[1] != null) MoveToDirectory = args[1];
-			if (args[2] != null) DelayTime = int.Parse(args[2]);
-			if (args[3] != null) FileMoveTime = int.Parse(args[3]);
+			if (args.Length > 0)
+			{
+				SourceDirectory = args[0];
+				if (args.Length > 1) MoveToDirectory = args[1];
+				if (args.Length > 2) DelayTime = int.Parse(args[2]);
+				//if (args.Length > 3) FileMoveTime = int.Parse(args[3]);
+			}
 
 			Timer timer = new();
 			timer.Elapsed += Timer_Elapsed;
 			timer.Interval = DelayTime * 60000;
 			timer.AutoReset = true;
+#if DEBUG
+			timer.Interval = 1000;
+			timer.AutoReset = false;
+#endif
 			Task.Run(() => timer.Start());
 			Console.WriteLine("Files will be checked every " + DelayTime + " minutes");
 			Console.ReadLine();
@@ -36,11 +43,13 @@ namespace FileMover
 			foreach (string file in Directory.GetFiles(SourceDirectory))
 			{
 				FileInfo fileInfo = new(file);
+				if (fileInfo.Extension != ".plot") continue;
+				File.Move(file, MoveToDirectory + fileInfo.Name); // Moved this line because only '.plot' files are being moved.
 
-				if (File.GetCreationTime(file).AddHours(FileMoveTime) < DateTime.Now)
-				{
-					File.Move(file, MoveToDirectory + fileInfo.Name);
-				}
+				//if (File.GetCreationTime(file).AddHours(FileMoveTime) < DateTime.Now)
+				//{
+				//	File.Move(file, MoveToDirectory + fileInfo.Name);
+				//}
 			}
 		}
 	}
